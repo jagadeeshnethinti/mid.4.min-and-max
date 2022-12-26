8844c4be0c70fefbd73e997a84cdfb3b0bc92a74
jagadeeshnethinti <109734275+jagadeeshnethinti@users.noreply.github.com>
2022-12-26 08:01:25 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = []
+    for i in range(n):
+        s=int(input())
+        arr.append(s)
+    Max=max(arr)
+    Min=min(arr)
+    print(min,max)
+    
